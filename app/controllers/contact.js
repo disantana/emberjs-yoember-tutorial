a9ac2d61b6dfc8aa767d00b979f48f6775c3795a
diego.pereira <diego.pereira@wexinc.com.br>
2018-03-13 17:11:04 -0300

    created contact form.   - Without validation in textarea

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,22 +1,21 @@
 import Controller from '@ember/controller';
-import { match, not } from '@ember/object/computed';
+import { match, not, gte } from '@ember/object/computed';
+
 
 export default Controller.extend({
-  // PROPERTIES OF VIEW
-  headerMessage: 'Coming Soon',
-
-  emailAddress: '',
+  message:'',
   isValid: match('emailAddress', /^.+@.+\..+$/),
   isDisabled: not('isValid'),
-
+  emailAddress:'',
+  isLongEnough: gte("message.length", 5),
 
   actions: {
    saveInvitation() {
      alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+     alert(`length: ${this.get('message').length}`)
      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
      this.set('emailAddress', '');
+     this.set('message', '');
    }
  }
-
-
 });
