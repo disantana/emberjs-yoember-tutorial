9319c5efe71dec4c432809c8d38b6f2afb5aac8d
diego.pereira <diego.pereira@wexinc.com.br>
2018-03-13 15:17:18 -0300

    Added functionality to valid an email input in the home page.

---
+++ app/controllers/index.js
@@ -0,0 +1,8 @@
+import Controller from '@ember/controller';
+import { match, not } from '@ember/object/computed';
+
+export default Controller.extend({
+  emailAddress: '',
+  isValid: match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: not('isValid')
+});
