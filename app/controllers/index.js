2625ff2cffb2d2fcd5968e0d5d19b3d3b50458a2
diego.pereira <diego.pereira@wexinc.com.br>
2018-03-13 15:46:34 -0300

    email validation and 'request invitation' action.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,7 +2,22 @@ import Controller from '@ember/controller';
 import { match, not } from '@ember/object/computed';
 
 export default Controller.extend({
+  // PROPERTIES OF VIEW
+  headerMessage: 'Coming Soon',
+
   emailAddress: '',
   isValid: match('emailAddress', /^.+@.+\..+$/),
-  isDisabled: not('isValid')
+  isDisabled: not('isValid'),
+
+
+  actions: {
+
+   saveInvitation() {
+     alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+     this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+     this.set('emailAddress', '');
+   }
+ }
+
+
 });
