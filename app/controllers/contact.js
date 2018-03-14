c7a1b698339c67d22a2c2b45b6729c5ed1eca551
diego.pereira <diego.pereira@wexinc.com.br>
2018-03-14 09:36:07 -0300

    Validation (maxlength)for textarea

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -7,12 +7,11 @@ export default Controller.extend({
   isValid: match('emailAddress', /^.+@.+\..+$/),
   isDisabled: not('isValid'),
   emailAddress:'',
-  isLongEnough: gte("message.length", 5),
+  maxLength: 280,
 
   actions: {
    saveInvitation() {
      alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
-     alert(`length: ${this.get('message').length}`)
      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
      this.set('emailAddress', '');
      this.set('message', '');
