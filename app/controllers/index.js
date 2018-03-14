8203aaabb564a6d44b3ce3eb92d6dd9577c90af8
diego.pereira <diego.pereira@wexinc.com.br>
2018-03-14 15:12:38 -0300

    add admin menu and page to list all invitations.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -12,17 +12,14 @@ export default Controller.extend({
 
   actions: {
     saveInvitation() {
-       var _that = this;
-       var email = this.get('emailAddress');
-       var newInvitation = this.store.createRecord('invitation', {
-             email: email
-        });
+      const email = this.get('emailAddress');
+      const newInvitation = this.store.createRecord('invitation', { email });
+      newInvitation.save().then(response => {
+        this.set('responseMessage', `Thank you! We saved your email address with the following id: ${response.get('id')}`);
+        this.set('emailAddress', '');
+      });
 
-       newInvitation.save().then(function(response) {
-          _that.set('responseMessage', "Thank you! We saved your email address with the following id: " + response.get('id'));
-          _that.set('emailAddress', '');
-       });
-   }
+    }
  }
 
 
